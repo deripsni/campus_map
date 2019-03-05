86eeabe5b4ef7efa02703d9ee270d74020379cb9
Dakota Maura <Dakota.Maura@gmail.com>
2019-03-04 23:01:57 -0800

    finished setting up backend
    moved some files around

--- campus_map.py
+++ campus_map.py
@@ -10,9 +10,5 @@ def index():
 def send_module(path):
     return send_from_directory('node_modules/leaflet/dist/', path)
 
-@app.route('/assets/<path:path>')
-def send_asset(path):
-    return send_from_directory('assets/', path)
-
 if __name__ == '__main__':
     app.run(debug=True)
