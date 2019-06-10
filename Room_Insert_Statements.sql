dc29f701b7cfcf99ab5fc89485ec7c04b8c42bbd
Brandon <brandonrich7@gmail.com>
2019-06-10 01:53:45 -0400

    Added some classrooms and locate features

--- Room_Insert_Statements.sql
+++ Room_Insert_Statements.sql
@@ -2,4 +2,12 @@ USE WWU;
 CREATE TABLE classrooms ( ROOM varchar(10), BUILDING varchar(5), X_COORD float, Y_COORD float, FLOOR int ); int, FLOOR int );
 INSERT INTO classrooms
 VALUES
-('ADM116', 'ADM', 3586, 5756.89, 1)
+('ADM116', 'ADM', 3586, 5756.89, 1),
+('ADM117', 'ADM', 3390, 5757.5, 1),
+('ADM006', 'ADM', 3563, 5606, 0),
+('CTC305', 'CTC', 1700, 2663, 3),
+('CTC314', 'CTC', 1759, 2370, 3),
+('CTC315', 'CTC', 1815, 2504, 3),
+('CTC316', 'CTC', 1977, 2401, 3),
+('CTC320', 'CTC', 2217, 2675, 3)
+
--- static/js/locate.js
+++ static/js/locate.js
@@ -2,6 +2,41 @@ $( 'document' ).ready(function() {
    $( 'ul.navbar-nav' ).on('click', 'a.locate', function() {
         $.get("http://localhost:5000/locate", {q: $(this).text()}, function(data, status) {
             if (status == 'success') {
+		//alert(data);
+		marker.setLatLng([data[0][0], data[0][1]]);
+		marker.addTo(map);
+		map.flyTo([data[0][0], data[0][1]], 0);
+		
+		//Current = Floor_1;
+		$('.leaflet-control-layers-selector')[data[0][2]].click();
+
+		
+		if (data[0][2] == 0){
+			Current=Floor_0;
+		}
+		else if (data[0][2] == 1){
+			Current=Floor_1;
+		}
+		else if (data[0][2] == 2){
+			Current=Floor_2;
+		}
+		else if (data[0][2] == 3){
+			Current=Floor_3;
+		}
+		
+		$('.leaflet-control-layers-selector')[data[0][2]].click();
+            }
+	    else{
+		alert('Send Help')
+		}
+        });
+    });
+});
+
+$( 'document' ).ready(function() {
+   $( '.room' ).click( function() {
+        $.get("http://localhost:5000/locate", {q: $(this).text()}, function(data, status) {
+            if (status == 'success') {
 	
 		marker.setLatLng([data[0][0], data[0][1]]);
 		marker.addTo(map);
